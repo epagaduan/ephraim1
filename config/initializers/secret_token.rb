d2f8e2328284a807f221438997c4ca8e3506fd67
ephraim pagaduan <ephraim@gdp@design.com>
2013-11-24 10:59:57 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ephraim1::Application.config.secret_key_base = '6281b9edf425f8c40cced69a13fd3d0d5a3486eef6d741a83d55a51b0a3208d7b81c6e91197218c0161f5c23d3e1ce1766c292c9bebd90047af7a97a6b49f060'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ephraim1::Application.config.session_store :cookie_store, key: '_ephraim1_session'
